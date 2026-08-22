3f70547094187bf4fb757de3ca7ebb68bc9aa363
amir hosein <amirmiri.edu@gmail.com>
2026-08-23 03:05:07 +0330

    use url template tag for dynamic url
    use url and name for path and add app_name to the url of app

--- website/urls.py
+++ website/urls.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 from django.urls import path
 from website.views import *
+app_name = "website"
 urlpatterns = [
-    path("",index_html),
-    path("contact/", index_contact),
-    path("about/", index_about)
+    path("",index_html , name= 'index'),
+    path("contact/", index_contact, name = 'contact'),
+    path("about/", index_about, name = 'about')
 
 ]
