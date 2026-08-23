d0b0cbc2b0a0099c084e7813e4867fdde4cd77c9
amir hosein <amirmiri.edu@gmail.com>
2026-08-23 16:35:11 +0330

    makemigrations he model of blog
    add post table and title , content fileds
    and add app blog to thec INSTALLED APPS

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,6 @@
 from django.db import models
-
+# data base --> create table the name is : app_name-class-name --> coulns --> filds
 # Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length= 255)
+    content = models.TextField()
