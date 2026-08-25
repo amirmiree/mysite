42a7fc14feab8bfe0db6641865f3435139c6bac0
amir hosein <amirmiri.edu@gmail.com>
2026-08-25 17:45:57 +0330

    match  database with html
    show posts details bsaed on the databse by template tag and repeat structure of html

--- blog/views.py
+++ blog/views.py
@@ -2,14 +2,16 @@ from django.shortcuts import render
 from blog.models import Post
 # Create your views here.
 def index_blog_html(request):
-    return render(request  ,"blog/blog-home.html" )
+    posts = Post.objects.all()
+    context = {"posts": posts}
+    return render(request  ,"blog/blog-home.html", context )
 
 def index_blog_single(request):
     context = {"name": "amir", "lastname": "miri"}
     return render(request ,"blog/blog-single.html", context )
 
-def test(request):
-    # get data from databse 
-    posts = Post.objects.all()
-    context = {"post": posts}
-    return render(request ,"test.html", context )
+# def test(request):
+#     # get data from databse 
+#     posts = Post.objects.all()
+#     context = {"post": posts}
+#     return render(request ,"test.html", context )
